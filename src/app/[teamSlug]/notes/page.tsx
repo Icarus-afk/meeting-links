cd3b5d2c2c0999d6fc6a19b4013ed63e409048ba
Sabbir Ahmed <sabbiirr@gmail.com>
2024-04-28 00:51:21 +0600

    added editor

---
+++ src/app/[teamSlug]/notes/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { Editor } from "@/components/Editor";
+
+export default function NotesHome() {
+  return (
+    <div className="h-screen flex">
+      <Editor />
+    </div>
+  );
+}
---
+++ src/components/Editor/index.tsx
@@ -0,0 +1,82 @@
+import { $getRoot, $getSelection } from "lexical";
+import { useEffect } from "react";
+
+import { AutoFocusPlugin } from "@lexical/react/LexicalAutoFocusPlugin";
+import { LexicalComposer } from "@lexical/react/LexicalComposer";
+import { RichTextPlugin } from "@lexical/react/LexicalRichTextPlugin";
+import { ContentEditable } from "@lexical/react/LexicalContentEditable";
+import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin";
+import LexicalErrorBoundary from "@lexical/react/LexicalErrorBoundary";
+import ToolbarPlugin from "./plugins/Toolbar";
+import "./editor.css";
+
+const theme = {
+  code: "editor-code",
+  heading: {
+    h1: "editor-heading-h1",
+    h2: "editor-heading-h2",
+    h3: "editor-heading-h3",
+    h4: "editor-heading-h4",
+    h5: "editor-heading-h5",
+  },
+  image: "editor-image",
+  link: "editor-link",
+  list: {
+    listitem: "editor-listitem",
+    nested: {
+      listitem: "editor-nested-listitem",
+    },
+    ol: "editor-list-ol",
+    ul: "editor-list-ul",
+  },
+  ltr: "ltr",
+  paragraph: "editor-paragraph",
+  placeholder: "editor-placeholder",
+  quote: "editor-quote",
+  rtl: "rtl",
+  text: {
+    bold: "editor-text-bold",
+    code: "editor-text-code",
+    hashtag: "editor-text-hashtag",
+    italic: "editor-text-italic",
+    overflowed: "editor-text-overflowed",
+    strikethrough: "editor-text-strikethrough",
+    underline: "editor-text-underline",
+    underlineStrikethrough: "editor-text-underlineStrikethrough",
+  },
+};
+
+// Catch any errors that occur during Lexical updates and log them
+// or throw them as needed. If you don't throw them, Lexical will
+// try to recover gracefully without losing user data.
+function onError(error: any) {
+  console.error(error);
+}
+
+function Placeholder() {
+  return <div className="editor-placeholder">Meeting Notes...</div>;
+}
+export function Editor() {
+  const initialConfig = {
+    namespace: "MyEditor",
+    theme,
+    onError,
+  };
+
+  return (
+    <LexicalComposer initialConfig={initialConfig}>
+      <div className="editor-container">
+        <ToolbarPlugin />
+        <div className="editor-inner">
+          <RichTextPlugin
+            contentEditable={<ContentEditable className="editor-input" />}
+            placeholder={<Placeholder />}
+            ErrorBoundary={LexicalErrorBoundary}
+          />
+          <HistoryPlugin />
+          <AutoFocusPlugin />
+        </div>
+      </div>
+    </LexicalComposer>
+  );
+}
